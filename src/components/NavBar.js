d364f62c46051943140fec4f855bb84f6474fd26
munroe1786 <era3munroe@gmail.com>
2021-10-29 19:11:34 -0500

    start building out navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, NavBar } from 'react-bootstrap';
+
+const NavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar
