2fefe1dc10c6970ddb25d10aa9339b9b562ab9cf
munroe1786 <era3munroe@gmail.com>
2021-11-02 21:45:58 -0500

    import Col and Row from react-bootstrap into Contacts.js and add contacts and loading state variables

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useEffect, useState} from 'react';
 import axios from 'axios';
+import { Col, Row } from 'react-bootstrap';
 
 const Contacts = () => {
+
+    const [contacts, setContacts] = useState([])
+    const [loading, setLoading] = useState(false);
+
     return (
         <div>
             
