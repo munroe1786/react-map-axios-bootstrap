f1f9416a5c1e11fba0607af191f8cf752ccc2ca5
munroe1786 <era3munroe@gmail.com>
2021-11-05 22:15:23 -0500

    add deconstructed contact prop to Contact.js and delete closing img tag since it's self-closing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap'; 
 
-const Contact = () => {
+const Contact = ({ contact }) => {
     return (
         <>
         <Card className='my-3 p-3 rounded h-90'>
-            <Card.Img style={{objectFit:"containt"}} variant='top' src={contact.picture.large}>
+            <Card.Img style={{objectFit:"containt"}} variant='top' src={contact.picture.large} />
 
-            </Card.Img>
+
         </Card>
         </>
     )
